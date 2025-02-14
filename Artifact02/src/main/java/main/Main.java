6e602554545546021b98fb3c05f24900ef827a85
VRlabel <VRlabel@TI-01.vrlabel.local>
2025-02-14 14:53:43 -0300

    Adicionado lógica com os Beans

--- Artifact02/src/main/java/main/Main.java
+++ Artifact02/src/main/java/main/Main.java
@@ -1,25 +1,20 @@
 package main;
 
 import model.Comment;
-import proxies.EmailCommentNotificationProxy;
-import repositories.DBCommentRepository;
 import services.CommentService;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import beans.Beans;
 
 public class Main {
 	public static void main(String[] args) {
-		var commentRepository =
-			new DBCommentRepository();
-		var commentNotificationProxy =
-			new EmailCommentNotificationProxy();
-		var commentService =
-			new CommentService(
-				commentRepository, commentNotificationProxy);
+		var config = new AnnotationConfigApplicationContext(Beans.class);
 		var comment = new Comment();
 		comment.setAuthor("Laurentiu");
 		comment.setText("Demo comment");
 		
+		var commentService = config.getBean(CommentService.class);
 		commentService.publishComment(comment);
-		System.out.println(comment.getAuthor());
 	}
 }
